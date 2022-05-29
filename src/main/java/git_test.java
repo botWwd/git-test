c7b479b807b75530c3424b4b97702efc5df3b978
dww <105301746+bootw@users.noreply.github.com>
2022-05-29 09:49:53 +0800

    pull test

--- src/main/java/git_test.java
+++ src/main/java/git_test.java
@@ -6,5 +6,6 @@ public class git_test {
         System.out.println("first git4");
         System.out.println("master test");
         System.out.println("master breach");
+        System.out.println("pull breach");
     }
 }
